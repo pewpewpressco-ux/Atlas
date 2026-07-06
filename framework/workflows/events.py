cbd6b67c2098441ac01e59143dfe09b9eeef6f44
pewpewpressco-ux <pewpewpress.co@gmail.com>
2026-07-05 21:34:42 -0400

    Create events.py

---
+++ framework/workflows/events.py
@@ -0,0 +1,17 @@
+class Event:
+
+    MARKET_OPEN = "market-open"
+
+    MARKET_CLOSE = "market-close"
+
+    MIDDAY = "midday"
+
+    WEEKLY_RESEARCH = "weekly-research"
+
+    MONTHLY_REVIEW = "monthly-review"
+
+    STRATEGY_FAILURE = "strategy-failure"
+
+    NEW_RESEARCH = "new-research"
+
+    PROMOTION = "promotion-review"
