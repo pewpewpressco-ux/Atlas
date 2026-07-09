f838306815f10b7fb72ea73a60c4971baede38be
pewpewpressco-ux <pewpewpress.co@gmail.com>
2026-07-08 21:31:07 -0400

    Adapt ArtifactFactory to typed Artifact model

--- framework/artifacts/factory.py
+++ framework/artifacts/factory.py
@@ -1,30 +1,19 @@
 from framework.artifacts.artifact import Artifact
+from framework.artifacts.enums import ArtifactType
 
 
 class ArtifactFactory:
 
     def create(
-
         self,
-
         identifier,
-
         title,
-
-        artifact_type,
-
-        **kwargs
-
-    ):
-
+        artifact_type: ArtifactType,
+        **kwargs,
+    ) -> Artifact:
         return Artifact(
-
             id=identifier,
-
             title=title,
-
             type=artifact_type,
-
-            **kwargs
-
+            **kwargs,
         )
