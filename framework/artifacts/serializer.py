78e450d7ec83faf94e868e7531d7dff6341bea4f
pewpewpressco-ux <pewpewpress.co@gmail.com>
2026-07-08 21:31:18 -0400

    Support immutable Artifact serialization

--- framework/artifacts/serializer.py
+++ framework/artifacts/serializer.py
@@ -1,16 +1,24 @@
 from dataclasses import asdict
+from enum import Enum
 
 import yaml
 
 
 class ArtifactSerializer:
 
+    def _normalize(self, value):
+        if isinstance(value, Enum):
+            return value.value
+        if isinstance(value, tuple):
+            return [self._normalize(item) for item in value]
+        if isinstance(value, dict):
+            return {key: self._normalize(val) for key, val in value.items()}
+        if hasattr(value, "__dataclass_fields__"):
+            return self._normalize(asdict(value))
+        return value
+
     def dump(self, artifact):
-
         return yaml.safe_dump(
-
-            asdict(artifact),
-
-            sort_keys=False
-
+            self._normalize(artifact),
+            sort_keys=False,
         )
